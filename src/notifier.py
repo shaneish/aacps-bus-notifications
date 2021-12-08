65d61e8e784dcc0a5fc39f27d811cefcf26cb061
metriczulu <stephenson.shane.a@gmail.com>
2021-12-07 21:36:35 -0500

    added schema validation and extra logging

--- src/notifier.py
+++ src/notifier.py
@@ -4,6 +4,8 @@ from pprint import pprint
 from twilio.rest import Client
 from configparser import ConfigParser
 import pathlib
+from datetime import datetime
+import re
 
 
 def format_notification(row, school):
@@ -14,6 +16,12 @@ def format_notification(row, school):
     time_slot = f"Time -- {row[3]}"
     impact = f"Impact -- {row[4]}"
     return "\n\n".join(["Affected Bus:", bus, time_slot, school, sub_bus, impact])
+
+ 
+def validate_data(raw_data):
+    data = raw_data.replace("\n", "").replace("\r", "").replace("\t", "")
+    cols = json.loads(("[" + ", ".join(re.findall(r"columns: \[(.*?)\]", data)) + "]").lower())
+    return cols, cols[0]['title'].strip() == 'bus' and cols[1]['title'].strip() == 'sub bus' and cols[2]['title'].strip() == 'schools' and cols[3]['title'].strip() == 'schedules' and cols[4]['title'].strip() == 'impact'
 
 
 def get_number_iterator(recipients_csv):
@@ -70,48 +78,58 @@ if __name__ == "__main__":
     # Welcome to the most dense, unpythonic code possible.
     # I know.  Sorry.
     # Extracts the table information from AACPS' bus website.
-    data = json.loads(
-        next(
-            filter(
-                lambda line: "var dataArray" in line,
-                requests.get(
-                    "https://busstops.aacps.org/public/BusRouteIssues.aspx"
-                ).text.split(
-                    "\n"
-                ),  # returns the line of the raw HTML that contains the table data
+    raw_data = requests.get(configs["general"]["site"]).text
+    cols, validation = validate_data(raw_data)
+
+    if validation:
+        data = json.loads(
+            next(
+                filter(
+                    lambda line: "var dataArray" in line,
+                    raw_data.split("\n")  # returns the line of the raw HTML that contains the table data
+                )
             )
+            .split("=")[-1]  # drops the "var dataArray = " part of the line
+            .strip()[:-1]  # removes the trailing bracket
+            .replace("'", '"')  # replaces single quotes with double quotes
         )
-        .split("=")[-1]  # drops the "var dataArray = " part of the line
-        .strip()[:-1]  # removes the trailing bracket
-        .replace("'", '"')  # replaces single quotes with double quotes
-    )
 
-    # create a mapping from bus number to all outages for that particular bus
-    message_map = dict()
-    for row in data:
-        school = row[2]
-        message_map[row[0]] = message_map.get(row[0], []) + [
-            format_notification(row, school)
-        ]
+        # log current schedule
+        with open(configs['general']['log'], 'a') as log:
+            log.write(f"\n{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}|{json.dumps(cols)}|{json.dumps(data)}\n")
 
-    # iterate over every recipient listed in the recipients file and send notification it here is an outage
-    for phone_num, bus_num, school, always_notify in get_number_iterator(
-        current_dir / "recipients.csv"
-    ):
-        try:
-            send_notification(
-                phone_num,
-                bus_num,
-                school,
-                always_notify,
-                message_map,
-                call_client,
-                configs["twilio"]["from_phone"],
-            )
-        except Exception as e:
-            print(f">>> Error: {e}")
-            call_client.messages.create(
-                body=f"Bus Error: {e} / Phone: {phone_num} / Bus: {bus_num} / School: {school}",
-                from_=configs["debug"]["from_phone"],
-                to=configs["debug"]["to_phone"],
-            )
+        # create a mapping from bus number to all outages for that particular bus
+        message_map = dict()
+        for row in data:
+            school = row[2]
+            message_map[row[0]] = message_map.get(row[0], []) + [
+                format_notification(row, school)
+            ]
+
+        # iterate over every recipient listed in the recipients file and send notification it here is an outage
+        for phone_num, bus_num, school, always_notify in get_number_iterator(
+            current_dir / configs['general']['users']
+        ):
+            try:
+                send_notification(
+                    phone_num,
+                    bus_num,
+                    school,
+                    always_notify,
+                    message_map,
+                    call_client,
+                    configs["twilio"]["from_phone"],
+                )
+            except Exception as e:
+                print(f">>> Error: {e}")
+                call_client.messages.create(
+                    body=f"Bus Error: {e} / Phone: {phone_num} / Bus: {bus_num} / School: {school}",
+                    from_=configs["debug"]["from_phone"],
+                    to=configs["debug"]["to_phone"],
+                )
+    else:
+       call_client.messages.create(
+                    body=f"Error: Table does not have proper schema.\n\n{json.dumps(cols)}",
+                    from_=configs["debug"]["from_phone"],
+                    to=configs["debug"]["to_phone"],
+                ) 
