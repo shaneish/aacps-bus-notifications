8d882a2fffe252fab38f986dbc8fc129c5887f4c
metriczulu <stephenson.shane.a@gmail.com>
2021-12-09 01:06:50 -0500

    added change notifications

--- src/scheduler.py
+++ src/scheduler.py
@@ -15,10 +15,10 @@ auth_token = configs["twilio"]["auth"]
 call_client = Client(os.environ[account_sid], os.environ[auth_token])
 
 
-def run_notifier(call_client=call_client):
+def run_notifier_compare(call_client=call_client):
     print("running notifier")
     notifier = pathlib.Path(__file__).parent / "notifier.py"
-    notifs = run(f"python {str(notifier)} --log", shell=True)
+    notifs = run(f"python {str(notifier)} -l -c", shell=True)
     if notifs.returncode != 0:
         print("failed")
         call_client.messages.create(
@@ -28,13 +28,26 @@ def run_notifier(call_client=call_client):
         )
 
 
-def weekday_schedule(times=()):
+def run_notifier_no_compare(call_client=call_client):
+    print("running notifier")
+    notifier = pathlib.Path(__file__).parent / "notifier.py"
+    notifs = run(f"python {str(notifier)} -l", shell=True)
+    if notifs.returncode != 0:
+        print("failed")
+        call_client.messages.create(
+            body=f"Error: Notifier failed with code {notifs.returncode}",
+            from_=configs["debug"]["from_phone"],
+            to=configs["debug"]["to_phone"],
+        )
+
+
+def weekday_schedule(times=(), notifier=run_notifier_compare):
     for time_slot in times:
-        schedule.every().monday.at(time_slot).do(run_notifier)
-        schedule.every().tuesday.at(time_slot).do(run_notifier)
-        schedule.every().wednesday.at(time_slot).do(run_notifier)
-        schedule.every().thursday.at(time_slot).do(run_notifier)
-        schedule.every().friday.at(time_slot).do(run_notifier)
+        schedule.every().monday.at(time_slot).do(notifier)
+        schedule.every().tuesday.at(time_slot).do(notifier)
+        schedule.every().wednesday.at(time_slot).do(notifier)
+        schedule.every().thursday.at(time_slot).do(notifier)
+        schedule.every().friday.at(time_slot).do(notifier)
 
 
 if __name__ == "__main__":
@@ -43,10 +56,10 @@ if __name__ == "__main__":
             configs["schedule"]["morning"],
             configs["schedule"]["mid_noon"],
             configs["schedule"]["late_noon"],
-            configs["schedule"]["evening"],
         )
     )
-    run_notifier()
+    weekday_schedule(times=(configs["schedule"]["evening"],), notifier=run_notifier_no_compare)
+    run_notifier_no_compare()
 
     while True:
         schedule.run_pending()
